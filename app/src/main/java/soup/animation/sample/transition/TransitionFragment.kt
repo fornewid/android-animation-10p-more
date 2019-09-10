4d920362c977df816c346495d2dc8e15317c8a57
Sungyong An <soupyong@gmail.com>
2019-09-10 19:46:22 +0900

    Add example for transition

--- app/src/main/java/soup/animation/sample/transition/TransitionFragment.kt
+++ app/src/main/java/soup/animation/sample/transition/TransitionFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.transition.AutoTransition
+import androidx.transition.TransitionManager
+import kotlinx.android.synthetic.main.fragment_transition.*
 import soup.animation.sample.R
 
 class TransitionFragment : Fragment() {
@@ -15,5 +19,24 @@ class TransitionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_transition, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener {
+            // val transition = TransitionInflater.from(it.context).inflateTransition(R.transition.auto_transition)
+            // TransitionManager.beginDelayedTransition(container, transition)
+            TransitionManager.beginDelayedTransition(container, AutoTransition())
+            outerBackground.switchVisibility()
+            innerBackground.switchVisibility()
+        }
+    }
+
+    private fun View.switchVisibility() {
+        visibility = if (this.isVisible) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
